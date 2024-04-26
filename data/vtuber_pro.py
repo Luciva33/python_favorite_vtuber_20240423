90a5c5902e81e1d2026fff1bb4e2501f7c2f9b60
sishikawa-rop2312 <155729870+sishikawa-rop2312@users.noreply.github.com>
2024-04-26 21:11:46 +0900

    スクレイピング

---
+++ data/vtuber_pro.py
@@ -0,0 +1,22 @@
+import urllib.parse
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path # フォルダ操作モジュール
+import urllib   # url操作モジュール
+import time # 時間操作モジュール
+
+load_url = 'https://hololive.hololivepro.com/talents/roboco-san/'
+res = requests.get(load_url)
+res.encoding = res.apparent_encoding
+soup = BeautifulSoup(res.text, 'html.parser')
+
+data_dts = soup.select('.talent_data dl dt')
+data_dds = soup.select('.talent_data dl dd')
+
+print('---------- dt ------------')
+for dt in data_dts:
+    print(dt.get_text())
+
+print('---------- dd ------------')
+for dd in data_dds:
+    print(str(dd))
