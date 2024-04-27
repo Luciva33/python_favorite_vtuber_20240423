c73fa5c56adb801536a5867721c31d0802d4988a
sishikawa-rop2312 <155729870+sishikawa-rop2312@users.noreply.github.com>
2024-04-27 20:45:16 +0900

    sql作成

--- data/vtuber_data.py
+++ data/vtuber_data.py
@@ -5,7 +5,7 @@ from pathlib import Path # フォルダ操作モジュール
 import urllib   # url操作モジュール
 import time # 時間操作モジュール
 
-load_url = 'https://hololive.hololivepro.com/talents/todoroki-hajime/'
+load_url = 'https://hololive.hololivepro.com/talents/harusaki-nodoka/'
 res = requests.get(load_url)
 res.encoding = res.apparent_encoding
 soup = BeautifulSoup(res.text, 'html.parser')
